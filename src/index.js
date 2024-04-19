26328aeafc5b50e38fd66044ab30ae1eb68d0be1
Sonal <sonal.jain6431@gmail.com>
2024-04-19 20:08:00 +0530

    custom api response & error

--- src/index.js
+++ src/index.js
@@ -5,4 +5,16 @@ dotenv.config({
     path: './env'
 })
 
-connectDB() 
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    app.on("error", (err) => {
+        console.log(`MongoDB connection failed with express!! ${err}`)
+        throw err 
+    })
+    console.log(`MongoDB connection failed!! ${err}`)
+}) 
