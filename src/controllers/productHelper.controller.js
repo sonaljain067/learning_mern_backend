ec6214eb23d596530f7fe27cbb04b4999c45d5d7
Sonal <sonal.jain6431@gmail.com>
2024-05-04 22:45:58 +0530

    modifications in product helper

--- src/controllers/productHelper.controller.js
+++ src/controllers/productHelper.controller.js
@@ -3,10 +3,13 @@ import { ApiError }from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { Product } from "../models/product.model.js"
 import { ProductAttribute } from "../models/productAttribute.model.js"
-import { ProductDetails } from "../models/productDetail.model.js"
+import { ProductDetail } from "../models/productDetail.model.js"
 
 const createProductAttribute = asyncHandler(async(req, res) => {
+    // input from frontend 
     const { type, value } = req.body 
+
+    // input check 
     if(!type) {
         throw new ApiError(400, "Type is required to create!")
     }
@@ -14,71 +17,93 @@ const createProductAttribute = asyncHandler(async(req, res) => {
         throw new ApiError(400, "Value is required to create!")
     }
 
-    const attributeExists = await ProductAttribute.findOne({ type }) 
+    // attribute exists check 
+    const attributeExists = await ProductAttribute.findOne({ value }) 
     if(attributeExists) {
         throw new ApiError(401, "Attribute already exists!")
     }
 
+    // attribute creation 
     const createdAttribute = await ProductAttribute.create({
         type, 
         value 
     })
 
+    // response return 
     return res.status(200)
         .json(new ApiResponse(200, createdAttribute, "Attribute created succesfully!"))
 })
 
 const fetchProductAttributes = asyncHandler(async(req, res) => {
+    // input from frontend 
     const { attributeId } = req.params 
+
+    // input check 
     if(!attributeId) {
         throw new ApiError(400, "Attribute is required to fetch!")
     }
+
+    // fetch attribute 
     const attribute = await ProductAttribute.findById(attributeId)
     if(!attribute) {
         throw new ApiError(404, "No such attribute exists!")
     } 
 
+    // response return 
     return res.status(200)
         .json(new ApiResponse(200, attribute, "Attribute fetched succesfully!"))
 })
 
 const updateProductAttribute = asyncHandler(async(req, res) => {
+    // input from frontend
     const { attributeId } = req.params 
     const { type, value } = req.body 
+
+    // input check 
     if(!attributeId) {
         throw new ApiError(400, "Attribute is required to update!")
     }
     if(!type) {
-        throw new ApiError(400, "Type is required to create!")
+        throw new ApiError(400, "Type is required to update!")
     }
     if(!value) {
-        throw new ApiError(400, "Value is required to create!")
+        throw new ApiError(400, "Value is required to update!")
     }
 
+    // attribute fetch 
     const attributeExists = await ProductAttribute.findById(attributeId)
     if(!attributeExists) {
         throw new ApiError(404, "No such attribute exists!")
     }
 
+    // attribute updation 
     const updatedAttribute = await ProductAttribute.findByIdAndUpdate(attributeId, {
         $set: {
             type, 
             value 
         }
+    }, {
+        new: true
     })
 
+    // response return 
     return res.status(200)
         .json(new ApiResponse(200, updatedAttribute, "Attribute created succesfully!"))
 })
 
-const deleteProductAttribute = asyncHandler(async(req, res) => {
+const deleteProductAttribute = asyncHandler(async(req, res) => {    
+    // input from frontend 
     const { attributeId } = req.params 
+    
+    // input check 
     if(!attributeId) {
         throw new ApiError(400, "Attribute is required to fetch!")
     }
 
+    // attribute deletion 
     const deletedAttribute = await ProductAttribute.findByIdAndDelete(attributeId)
 
+    // response return 
     if(deletedAttribute){
         return res.status(200)
         .json(new ApiResponse(200, {}, "Attribute deleted succesfully!"))
@@ -89,132 +114,166 @@ const deleteProductAttribute = asyncHandler(async(req, res) => {
 })
 
 const createProductDetails = asyncHandler(async(req, res) => {
+    // input from frontend 
     const { productId } = req.params 
     const { size, color, price, qty } = req.body 
 
+    // input check
     if(!productId) {
         throw new ApiError(400, "Product is required to create details!")
     }
-
     if(!(price && qty && size && color)) {
         throw new ApiError(400, "All fields are required to create product details!")
     }
 
-    const productSize = await ProductAttribute.findOne({ value: size })
-    
-    const productColor = await ProductAttribute.findOne({ value: color })
-
-    if(!productType) {
-        throw new ApiError(404, "No such type exists!")
+    // product fetch 
+    const product = await Product.findById(productId) 
+    if(!product) {
+        throw new ApiError(400, "No such product exists!")
     }
+
+    // color & size attribute fetch 
+    let productSize = await ProductAttribute.findOne({ 
+        type: "size", value: size 
+    })
+    
+    let productColor = await ProductAttribute.findOne({ 
+        type: "color", value: color 
+    })
+
+    // creation if color & size doesn't exist 
+    if(!productSize) {
+        productSize = await ProductAttribute.create({ 
+            type: "size", value: size 
+        })
+    }
+    if(!productColor) {
+        productColor = await ProductAttribute.create({ 
+            type: "color", value: color
+        })
+    }
+
+    // < 0 price check 
     if(price <= 0) {
         throw new ApiError(400, "Price should be greater than 0!")
-    } 
+    }   
 
-    const createdDetails = await ProductDetails.create({
-        productType, 
-        productSize,
+    // detail exist check 
+    const isProductDetailExists = await ProductDetail.find({
+        product, 
+        productSize, 
         productColor, 
         price, 
         qty 
-    })
+    })  
 
-    return res.status(200)
-        .json(new ApiResponse(200, createdDetails, "Product details created succesfully!"))
-
+    // response return 
+    if(isProductDetailExists.length > 0){
+        throw new ApiError(400, "Product details already exists!")
+    } else {
+        const createdDetails = await ProductDetail.create({
+            product,
+            productSize,
+            productColor, 
+            price, 
+            qty 
+        })
+        return res.status(200)
+            .json(new ApiResponse(200, createdDetails, "Product details created succesfully!"))
+    }
 })
 
 const updateProductDetails = asyncHandler(async(req, res) => {
-    const { productId } = req.params 
-    if(!productId) {
+    // input from frontend 
+    const { productDetId } = req.params 
+    let { size, color, price, qty } = req.body 
+
+    // input check 
+    if(!productDetId) {
         throw new ApiError(400, "Product is required to fetch product details!")
     }
-
-    const { size, color, price, qty } = req.body 
     if(!(size || color || price || qty)) {
         throw new ApiError(400, "Details cannot be empty to update product details!")
     }
 
-    const product = await Product.findById(productId)
-    if(!product) {
-        throw new ApiError(404, "No such product exists!")
-    } 
-
-    const productDetail = await ProductDetails.findOne({ product })
+    // fetch product detail 
+    const productDetail = await ProductDetail.findById(productDetId)
     if(!productDetail) {
         throw new ApiError(404, "No such product details exists!")
     }
 
-    const productSize = await ProductAttribute.find({ type: "size", value: size }) 
+    // fetch size & color 
+    let productSize = await ProductAttribute.findOne({ 
+        type: "size", value: size 
+    }) 
+    let productColor = await ProductAttribute.findOne({ type: "color", value: color }) 
 
-    const productColor = await ProductAttribute.find({ type: "color", value: color }) 
+    // creation if color & size doesn't exist 
+    if(!productSize) {
+        productSize = await ProductAttribute.create({
+            type: "size", value: size
+        })
+    }
+    if(!productColor) {
+        productColor = await ProductAttribute.create({
+            type: "color", value: color
+        })
+    }
 
-    // if(!productSize) {
-    //     productSize = productDetail.productSize
-    // }
-    // if(!productColor) {
-    //     productColor = productDetail.productColor
-    // }
-    // if(!price) {
-    //     price = productDetail.price 
-    // }
-    // if(!qty) {
-    //     qty = productDetail.qty 
-    // }
-
-    const updatedProductDetails = await ProductDetails.findByIdAndUpdate(productDetail._id, {
+    // product detail updation 
+    const updatedProductDetails = await ProductDetail.findByIdAndUpdate(productDetId, {
         $set: {
             productSize, 
             productColor, 
             price, 
-            qty, 
+            qty
         }
+    }, {
+        new: true 
     })
-    
+
+    // response return 
     return res.status(200)
-        .json(new ApiResponse(200, updatedProductDetails, "Attribute fetched succesfully!"))
+        .json(new ApiResponse(200, updatedProductDetails, "Attribute updated succesfully!"))
 })
 
-const fetchProductDetails = asyncHandler(async(req, res) => {
+const fetchAllProductDetails = asyncHandler(async(req, res) => {
+    // input from frontend 
     const { productId } = req.params 
+
+    // field check 
     if(!productId) {
         throw new ApiError(400, "Product is required to fetch product details!")
     }
 
+    // product fetch 
     const product = await Product.findById(productId)
     if(!product) {
         throw new ApiError(404, "No such product exists!")
     } 
 
-    const productDetail = await ProductDetails.findOne({ product })
-    if(!productDetail) {
-        throw new ApiError(404, "No such product details exists!")
-    }
+    // list of product details of product fetch 
+    const productDetails = await ProductDetail.find({ product })
 
-    if(productDetail) {
-        return res.status(200)
-            .json(new ApiResponse(200, productDetail, "Product details fetched succesfully!"))
-    } else {
-        return res.status(200)
-            .json(new ApiResponse(200, {}, "No product details exists with provided product Id!"))
-    }
-
+    // response return 
+    return res.status(200)
+        .json(new ApiResponse(200, productDetails, "Product details fetched succesfully!"))
 
 })
 
 const deleteProductDetails = asyncHandler(async(req, res) => {
-    const { productId } = req.params 
-    if(!productId) {
-        throw new ApiError(400, "Product is required to fetch product details!")
+    // input from frontend 
+    const { productDetId } = req.params 
+    
+    // field check 
+    if(!productDetId) {
+        throw new ApiError(400, "Product Detail is required to delete it!")
     }
 
-    const product = await Product.findById(productId)
-    if(!product) {
-        throw new ApiError(404, "No such product exists!")
-    } 
+    // product detail deletion 
+    const deletedProduct = await ProductDetail.findByIdAndDelete(productDetId)
 
-    const deletedProduct = await ProductDetails.findByIdAndDelete({ product })
-    
+    // response return 
     if(deletedProduct) {
         return res.status(200)
             .json(new ApiResponse(200, {}, "Product Details deleted succesfully!"))
@@ -225,4 +284,4 @@ const deleteProductDetails = asyncHandler(async(req, res) => {
 })
 
 
-export { createProductAttribute, fetchProductAttributes, updateProductAttribute, deleteProductAttribute, createProductDetails, fetchProductDetails, updateProductDetails, deleteProductDetails }
+export { createProductAttribute, fetchProductAttributes, updateProductAttribute, deleteProductAttribute, createProductDetails, fetchAllProductDetails, updateProductDetails, deleteProductDetails }
--- src/routes/productHelper.route.js
+++ src/routes/productHelper.route.js
@@ -1,23 +1,28 @@
 import Router from "express" 
-import { createProductAttribute, createProductDetails, deleteProductAttribute, deleteProductDetails, fetchProductAttributes, fetchProductDetails, updateProductAttribute, updateProductDetails } from "../controllers/productHelper.controller"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { createProductAttribute, createProductDetails, deleteProductAttribute, deleteProductDetails, fetchProductAttributes, fetchAllProductDetails, updateProductAttribute, updateProductDetails } from "../controllers/productHelper.controller.js"
 
 const router = Router() 
 
 router.use(verifyJWT)
 
-router.route("/a/:attributeId")
-    .post(createProductAttribute)
-
+// TODO: testing below routes
 router.route("/a")
+    .post(createProductAttribute)
+    
+
+router.route("/a/:attributeId")
     .get(fetchProductAttributes)
     .patch(updateProductAttribute)
-    .delete(deleteProductAttribute)
+    .delete(deleteProductAttribute) 
 
+router.route("/d/")
+    
 router.route("/d/:productId")
     .post(createProductDetails)
-
-router.route("/d")
-    .get(fetchProductDetails)
+    .get(fetchAllProductDetails)
+    
+router.route("/ud/:productDetId")
     .patch(updateProductDetails)
     .delete(deleteProductDetails)
 
